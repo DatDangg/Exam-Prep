dbf12851cd28001d45b6de586227e4f7749ff7e8
DatDangg <datdang20112003@gmail.com>
2025-04-10 02:29:42 +0700

    update small things

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,6 +1,25 @@
+import styles from './Home.module.css'
+
 function Home() {
     return (
-        <h1> HIIIIIIIIIIII</h1>
+        <div className={styles.home}>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-lg-12'>
+                        <h1>Thi thử trực tuyến miễn phí</h1>
+                        <p>Proident anim enim enim pariatur enim incididunt non minim sunt id fugiat duis ipsum.</p>
+                        <button>Vào thi ngay</button>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-lg-12'>
+                        <h1>Thi thử trực tuyến miễn phí</h1>
+                        <p>Proident anim enim enim pariatur enim incididunt non minim sunt id fugiat duis ipsum.</p>
+                        <button>Vào thi ngay</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
