702728999653bdd50d25f8a8c52e26d208d887f9
DatDangg <datdang20112003@gmail.com>
2025-04-22 00:13:31 +0700

    🛠️ add @EntityScan to detect Exam entity

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -2,12 +2,13 @@ package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.example.backend.model") // 🟢 QUAN TRỌNG
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
